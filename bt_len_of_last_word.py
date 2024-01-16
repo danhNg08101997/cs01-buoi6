16c38b44b1d761ae19e83ea69cd14a860e4b5b68
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-01-16 10:36:42 +0700

    in chuỗi số nguyên tố

---
+++ bt_len_of_last_word.py
@@ -0,0 +1,1 @@
+# Cho 1 chuỗi chứa các ký tự và khoảng trắng, trả về độ dài của từ cuối cùng trong chuỗi. Nếu từ cuối cùng không tồn tại trả về 0. Một từ được định nghĩa là một chuỗi ký tự liên tiếp không chứa khoảng trắng
--- in_chuoi_so_nguyen_to.py
+++ in_chuoi_so_nguyen_to.py
@@ -1,2 +1,13 @@
 # Viết chương trình cho phép người dùng nhập vào chuỗi số yêu cầu in ra các chữ số trong chuỗi là số nguyên tố, nếu không có số nguyên tố nào trong chuỗi thì in ra kết quả là không có số nguyên tố nào cả
+import method
 
+# Input
+str_input = input("Nhập vào chuỗi số: ")
+# Output
+output = ''
+# Process
+for str_num in str_input:
+    num = int(str_num)
+    if method.kiem_tra_so_nguyen_to(num):
+        output += f'{num} '
+print(f'Các số nguyên tố của dãy số {str_input} là: {output}')

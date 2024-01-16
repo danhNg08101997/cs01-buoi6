0a558704e6e030c9ce625ce9424bbf34a33317fc
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-01-16 11:05:52 +0700

    in ra độ dài từ cuối cùng

--- bt_len_of_last_word.py
+++ bt_len_of_last_word.py
@@ -1,1 +1,12 @@
 # Cho 1 chuỗi chứa các ký tự và khoảng trắng, trả về độ dài của từ cuối cùng trong chuỗi. Nếu từ cuối cùng không tồn tại trả về 0. Một từ được định nghĩa là một chuỗi ký tự liên tiếp không chứa khoảng trắng
+
+# Input
+str_input = input('Nhập chuỗi ký tự: ')
+# Output
+output = 0
+# Process
+new_string = str_input.split(" ")
+
+last_word = new_string[len(new_string) - 1]
+output = len(last_word)
+print(output)
